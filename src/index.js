2792aa910b3173be780e3453cdf1c26cf83c1eb8
Harsimar Ahluwalia <harsimar.ahluwalia@gmail.com>
2020-02-07 18:45:30 -0500

    Added eslint config and scripts to project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+
 const app = express();
 const port = 3001 || process.env.port;
 
 app.get('/', (req, res) => res.send('Hello World.'));
 
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`App listening on port ${port}`));
